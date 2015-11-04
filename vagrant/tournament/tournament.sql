8792fba1a2ff6f803ffeed11a7a184fdc7e216ce
Ryan Rustong <ryanr@Ryans-MBP.randm.local>
2015-11-03 23:02:58 -0800

    Attempt to create a "wins" view.  Needs more work.

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -13,7 +13,7 @@ CREATE DATABASE tournament;
 
 CREATE TABLE players
   (id serial,
-   name char(40));
+    name char(40));
 
 CREATE TABLE matches
   (id serial,
@@ -21,3 +21,26 @@ CREATE TABLE matches
   player1 smallint,
   player2 smallint,
   winner smallint);
+
+CREATE VIEW wins AS
+  SELECT
+    players.id, count(p1.winner), count(p2.winner)
+  FROM
+    players
+  LEFT JOIN
+    matches p1 ON (players.id = p1.player1)
+  LEFT JOIN
+    matches p2 ON (players.id = p2.player2)
+  GROUP BY
+    players.id
+
+CREATE VIEW wins2 AS
+    SELECT
+      players.id, players.name, count(matches.winner) AS wins, count(*) AS matches
+    FROM
+      players
+    LEFT JOIN
+      matches ON (players.id = matches.winner)
+    GROUP BY
+      players.id, players.name
+    ORDER BY wins DESC;
