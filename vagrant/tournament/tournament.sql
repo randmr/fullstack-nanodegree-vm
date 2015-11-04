273b47b507a4885ed46972497ce05cc399f72fe8
Ryan Rustong <rrustong@ragingwire.com>
2015-11-03 16:29:16 -0800

    initial database creation

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,18 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP DATABASE IF EXISTS tournament;
 
+CREATE DATABASE tournament;
+\c tournament
+
+CREATE TABLE players
+  (id serial,
+   name char(40));
+
+CREATE TABLE matches
+  (id serial,
+  round smallint,
+  player1 smallint,
+  player2 smallint,
+  winner smallint);
