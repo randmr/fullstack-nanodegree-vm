490442e7aa7be1d3ed2baeecbde0ec749e050736
Ryan Rustong <ryanr@Ryans-MBP.randm.local>
2015-11-03 23:10:56 -0800

    successfully added deleteMatches, deletePlayers, countPlayers, registerPlayer routines.  Attempted playerStandings, but not working yet.

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -13,25 +13,47 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    db = connect()
+    c = db.cursor()
+    c.execute("DELETE FROM matches;")
+    db.commit()
+    db.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    db = connect()
+    c = db.cursor()
+    c.execute("DELETE FROM players;")
+    db.commit()
+    db.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    db = connect()
+    c = db.cursor()
+    c.execute("SELECT count(id) FROM players;")
+    #count = ({'count': str(row[0])} for row in c.fetchall())
+    count = c.fetchone()
+    db.close()
+    return count[0]
+
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
+    db = connect()
+    c = db.cursor()
+    c.execute("INSERT INTO players (name) VALUES (%s)", (name,))
+    db.commit()
 
 
 def playerStandings():
@@ -47,6 +69,9 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    db = connect()
+    c = db.cursor()
+    c.execute("SELECT players.id, players.name, count(matches.winner) AS wins, count(*) AS matches FROM players LEFT JOIN matches ON (players.id = matches.winner) GROUP BY players.id, players.name ORDER BY wins DESC;")
 
 
 def reportMatch(winner, loser):
@@ -56,16 +81,16 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +98,3 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
-
